9aac9dfc5a9cbfba4a1f4c0899a5bd90a78d1d4f
Matheus Luiz <matheus.lopes@gcbinvestimentos.com>
2021-02-06 00:04:25 -0300

    adicionar rotas de login e alunos e criação do componente(pagina)

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import { Switch } from 'react-router-dom';
 import Route from './Route';
-import Users from '../pages/Users';
-import User from '../pages/User';
-import CopyRight from '../components/CopyRight';
+import Login from '../pages/Login';
+import AdicionarAluno from '../pages/alunos/adicionar';
+import ListarAlunos from '../pages/alunos/listar';
 
 export default function Routes() {
   return (
     <>
       <CopyRight />
       <Switch>
-        <Route path="/" exact component={Users} />
-        <Route path="/user/:user" exact component={User} />
+        <Route path="/login" exact component={Login} />
+        <Route path="/alunos/" exact component={ListarAlunos} />
+        <Route path="/alunos/adicionar" component={AdicionarAluno} />
       </Switch>
     </>
   );
